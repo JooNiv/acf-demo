d84e057b1343c58869bcad3fdceee931e3f8eaca
Joonas Nivala <joonas.nivala@gmail.com>
2025-10-07 19:30:55 +0300

    remove strictmode

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-
-    <div className='border-b-4 border-[#006778ff]'>
+  //<StrictMode>
+    <div>
+      <div className='border-b-4 border-[#006778ff]'>
       <h1 className="m-6 text-4xl font-bold">Quantum Bell State Simulator</h1>
     </div>
     <App />
-  </StrictMode>,
+    </div>
+  //</StrictMode>,
 )
