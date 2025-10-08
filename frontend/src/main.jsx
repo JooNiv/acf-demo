8efd0bfadc7339424bbed4de003817143937982c
Joonas Nivala <joonas.nivala@gmail.com>
2025-10-08 10:42:43 +0300

    link to fiqci status page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { CButton } from '@cscfi/csc-ui-react'
 import App from './App.jsx'
 
 // metadata
@@ -8,11 +9,18 @@ document.title = 'Quantum Bell State Simulator'
 
 createRoot(document.getElementById('root')).render(
   //<StrictMode>
-    <div>
-      <div className='border-b-4 border-[#006778ff]'>
+  <div>
+    <div className='flex flex-row border-b-4 border-[#006778ff] justify-between items-center'>
       <h1 className="m-6 text-4xl font-bold">Quantum Bell State Simulator</h1>
+      <CButton
+        type="button"
+        className='flex items-center m-6'
+        onClick={() => window.open('https://fiqci.fi/status', '_blank')}
+      >
+        Calibration Data
+      </CButton>
     </div>
     <App />
-    </div>
+  </div>
   //</StrictMode>,
 )
